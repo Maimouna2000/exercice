73b6ef19cbe79888fa46ef01d13ac26a850eee5a
maimouna <maifall26@gmail.com>
2023-11-27 16:46:43 +0100

    c'est mon nouveau

---
+++ exo1.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Tableau en php</title>   
+
+    </head>
+    <body>
+        <?php 
+            //affichage du tableau
+
+       $tableau= array(1,2,3,4,5,6);
+       
+
+       
+       
+       foreach ($tableau as $element){
+        echo  $element .':'.$info."<br/>";
+        
+       }
+        
+ 
+
+      
+       ?>
+    </body> 
+</html>
+
